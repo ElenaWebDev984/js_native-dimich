09f44faa8f163e5b30eb5aae4f93f5b98d7d28e5
ElenaWebDev984 <badalova.dev.984@gmail.com>
2025-10-18 23:44:24 +0200

    add tests for greating

---
+++ src/05-map/05.test.ts
@@ -0,0 +1,22 @@
+import type {ManType} from "./05.ts";
+import {beforeEach, expect, test} from "vitest";
+
+let people: ManType[] = []
+
+beforeEach( () => {
+    people = [
+        {name: 'Andrew Ivanov', age: 33},
+        {name: 'Alexander Petrov', age: 24},
+        {name: 'Dmitry Sidorov', age: 18},
+    ]
+})
+
+test('should get array of greating messages', () => {
+
+    const messages = people.map(man => `Hello ${man.name.split(' ')[0]}. Welcome to IT-Incubator`)
+
+    expect(messages.length).toBe(3)
+    expect(messages[0]).toBe('Hello Andrew. Welcome to IT-Incubator')
+    expect(messages[1]).toBe('Hello Alexander. Welcome to IT-Incubator')
+    expect(messages[2]).toBe('Hello Dmitry. Welcome to IT-Incubator')
+})
---
+++ src/05-map/05.ts
@@ -0,0 +1,65 @@
+export type ManType = {
+    name: string
+    age: number
+}
+
+const people: ManType[] = [
+    {name: 'Andrew Ivanov', age: 33},
+    {name: 'Alexander Petrov', age: 24},
+    {name: 'Dmitry Sidorov', age: 18},
+]
+
+
+const dimichTransformator = (man: ManType) => ({
+        stack: ['css, html', 'js', 'tdd', 'react'],
+        firstName: man.name.split(' ')[0],
+        lastName: man.name.split(' ')[1],
+    })
+
+
+const devs = [
+    {
+        stack: ['css, html', 'js', 'tdd', 'react'],
+        firstName: 'Andrew',
+        lastName: 'Ivanov',
+    },
+    {
+        stack: ['css, html', 'js', 'tdd', 'react'],
+        firstName: 'Alexander',
+        lastName: 'Petrov',
+    },
+    {
+        stack: ['css, html', 'js', 'tdd', 'react'],
+        firstName: 'Dmitry',
+        lastName: 'Sidorov',
+    },
+]
+
+
+
+// let d1 = dimichTransformator(people[0])
+// let d2 = dimichTransformator(people[1])
+// let d3 = dimichTransformator(people[2])
+//
+// const dev2 = [
+//     d1, d2, d3,
+// ]
+
+const devs2 = [
+    dimichTransformator(people[0]),
+    dimichTransformator(people[1]),
+    dimichTransformator(people[2]),
+]
+
+const devs3 = people.map(dimichTransformator)
+
+const devs4 = people.map(man => ({
+    stack: ['css, html', 'js', 'tdd', 'react'],
+    firstName: man.name.split(' ')[0],
+    lastName: man.name.split(' ')[1],
+}))
+
+
+
+const messages = people.map(man => `Hello ${man.name.split(' ')[0]}. Welcome to IT-Incubator`)
+
